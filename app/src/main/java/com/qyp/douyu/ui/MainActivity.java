abf9ff4c9fce4dd28dbb8d848fb0bbc05fa11d24
qianyipeng <loveqianwithyang@gmail.com>
2017-03-13 11:14:20 +0800

    测试提交

---
+++ app/src/main/java/com/qyp/douyu/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.qyp.douyu.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by qianyipeng on 2017/3/13.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
